364d6acfa5752138b7db70bfa60995d2698c06c5
ksiomng <202024007@kangwon.ac.kr>
2023-10-10 01:03:57 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -0,0 +1,18 @@
+import Foundation
+//
+// < 처음풀이 >
+// func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
+//    let cnt = lottos.filter{win_nums.contains($0)}.count
+//    let zcnt = lottos.filter{$0 == 0}.count
+//    let min = cnt > 0 ? 7 - cnt : 6
+//    let max = zcnt > 0 ? 7 - (cnt+zcnt) : min
+//    return [max,  min]
+// }
+
+// < 리팩토링 >
+func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
+  let cnt = lottos.filter{win_nums.contains($0)}.count
+  let zcnt = lottos.filter{$0 == 0}.count
+  let ranking = [6, 6, 5, 4, 3, 2, 1]
+  return [ranking[cnt+zcnt],  ranking[cnt]]
+}
