faecf5b09e92e93671d36f785ebd54b84a8d5759
ksiomng <202024007@kangwon.ac.kr>
2023-10-11 16:22:45 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
@@ -1,27 +1,44 @@
-import Foundation
+//import Foundation
 
-import Foundation
+// < 처음풀이 >
+//func solution(_ nums:[Int]) -> Int {
+//    var arrs = [Int]()
+//    var cnt = 0
+//    
+//    for i in 0..<nums.count-2 {
+//        for j in i+1..<nums.count-1 {
+//            for k in j+1..<nums.count {
+//                    arrs.append(nums[i] + nums[j] + nums[k])
+//            }
+//        }
+//    }
+//    for i in 0..<arrs.count {
+//        var bool = true
+//        
+//        for j in 2..<arrs[i] {
+//                if arrs[i] % j == 0 { bool = false }
+//        }
+//        if bool == true {
+//            cnt += 1
+//        }
+//    }
+//    return cnt
+//}
+
+// < 리팩토링 >
+func isPrime(_ num:Int) -> Bool {
+    for i in 2..<num { if num % i == 0 { return false } }
+    return true
+}
 
 func solution(_ nums:[Int]) -> Int {
-    var arrs = [Int]()
-    var cnt = 0
-    
+    var answer = 0
     for i in 0..<nums.count-2 {
         for j in i+1..<nums.count-1 {
             for k in j+1..<nums.count {
-                    arrs.append(nums[i] + nums[j] + nums[k])
+                if isPrime(nums[i]+nums[j]+nums[k]) { answer += 1 }
             }
         }
     }
-    for i in 0..<arrs.count {
-        var bool = true
-        
-        for j in 2..<arrs[i] {
-                if arrs[i] % j == 0 { bool = false }
-        }
-        if bool == true {
-            cnt += 1
-        }
-    }
-    return cnt
+    return answer
 }
