ecf97939e14f2eee31453de4c7d15db7c0e82681
ksiomng <202024007@kangwon.ac.kr>
2023-10-11 12:01:30 +0900

    [level 1] Title: 과일 장수, Time: 0.12 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -1,16 +1,24 @@
 import Foundation
 
+// < 처음풀이 >
+//func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
+//    var scores = score
+//    var answer = 0
+//    scores.sort()
+//    
+//    for _ in 0..<score.count % m {
+//        scores.removeFirst()
+//    }
+//    
+//    for i in 0..<(scores.count/m) {
+//        answer += scores[i*m] * m
+//    }
+//    return answer
+//}
+
+// < 리팩토링 >
 func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
-    var scores = score
-    var answer = 0
-    scores.sort()
-    
-    for _ in 0..<score.count % m {
-        scores.removeFirst()
-    }
-    
-    for i in 0..<(scores.count/m) {
-        answer += scores[i*m] * m
-    }
-    return answer
+    let scores = score.sorted(by: >)
+    return stride(from: m-1, to: scores.count, by: m)
+        .reduce(0, {$0 + scores[$1] * m}) //$0 초기값: m-1 $1 다음에 오는값: m
 }
