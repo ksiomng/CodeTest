f6eb90b080d07732b5d6b66f455d92814340b524
ksiomng <202024007@kangwon.ac.kr>
2023-10-14 16:32:10 +0900

    [level 1] Title: 소수 찾기, Time: 173.57 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.swift
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.swift
@@ -1,15 +1,31 @@
 import Foundation
 
-func isPrimeNumber(n: Int) -> Bool { // 소수판별 함수, 자기 자신의 제곱근 까지만 확인하면 소수 판별가능
-    if n < 2 {
-        return false
-    }
-    for i in 2..<Int(sqrt(Double(n)) + 1) {
-        if n % i == 0 { return false }
-    }
-    return true
-}
+// < 처음풀이 >
+// func isPrimeNumber(n: Int) -> Bool { // 소수판별 함수, 자기 자신의 제곱근 까지만 확인하면 소수 판별가능
+//     if n < 2 {
+//         return false
+//     }
+//     for i in 2..<Int(sqrt(Double(n)) + 1) {
+//         if n % i == 0 { return false }
+//     }
+//     return true
+// }
 
+// func solution(_ n:Int) -> Int {
+//     return Array(1...n).filter{isPrimeNumber(n: $0)}.count
+// }
+
+// < 리팩토링 >
 func solution(_ n:Int) -> Int {
-    return Array(1...n).filter{isPrimeNumber(n: $0)}.count
+    var primes:[Bool] = [Bool](repeating:false, count:n+1);
+    var count = 0;
+    for i in 2...n {
+        if(!primes[i]){
+            count = count + 1;
+        }
+        for j in 1...(n/i) {
+            primes[i*j]=true;
+        }
+    }
+    return count;
 }
