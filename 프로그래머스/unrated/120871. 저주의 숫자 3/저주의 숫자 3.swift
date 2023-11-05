fd42c5cc32fa22f9f9e94bbedd2ba1689ea82a35
ksiomng <202024007@kangwon.ac.kr>
2023-11-05 16:04:30 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.07 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n: Int) -> Int {
+    var answer = 0
+
+    for i in 1..<n + 1 {
+        answer += 1
+        while answer % 3 == 0 || String(answer).contains("3") {
+            answer += 1
+        }
+    }
+    return answer
+}
