36b1f75cfd21c9f0743259e5742a2c0d81e0f954
ksiomng <202024007@kangwon.ac.kr>
2023-11-06 10:10:58 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.10 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.swift
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.swift
@@ -1,20 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var result = [Int]()
-    if n%2 == 0 {
-        var i = 0
-        while i <= n {
-            result.append(i*i)
-            i += 2
-        }
-        return result.reduce(0, +)
-    } else {
-        var i = 1
-        while i <= n {
-            result.append(i)
-            i += 2
-        }
-        return result.reduce(0, +)
-    }
+    return n % 2 == 0 ? (1...n).filter{$0 % 2 == 0}.reduce(0) { $0 + $1 * $1 } : (1...n).filter{$0 % 2 == 1}.reduce(0, +)
 }
