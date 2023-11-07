412df5a8bdac10bf49dbfaf76f9da7f4eb5b8777
ksiomng <202024007@kangwon.ac.kr>
2023-11-07 10:00:59 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ s1:[String], _ s2:[String]) -> Int {
+    var cnt = 0
+    for i in s1 {
+        for j in s2 {
+            if i == j {cnt += 1}
+        }
+    }
+    return cnt
+}
