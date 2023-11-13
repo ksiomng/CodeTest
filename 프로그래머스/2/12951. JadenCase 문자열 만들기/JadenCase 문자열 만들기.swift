c91864af48148273b6a6ce6a88017657d13abc17
ksiomng <202024007@kangwon.ac.kr>
2023-11-13 11:31:46 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ s:String) -> String {
+    var arr = Array(s)
+    var result = ""
+    for i in 0..<arr.count {
+        if arr[i].isUppercase {
+            arr[i] = Character(String(arr[i].lowercased()))
+        }
+        if i == 0 {
+            arr[i] = Character(String(arr[i].uppercased()))
+        }
+        else {
+            if arr[i-1] == " " {
+                arr[i] = Character(String(arr[i].uppercased()))
+            }   
+        }
+    }
+    return String(arr)
+}
