0d3d44ddf367ad30ae40b620629cd7132bef287f
ksiomng <202024007@kangwon.ac.kr>
2023-11-13 10:13:26 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.64 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.swift
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.swift
@@ -1,9 +1,5 @@
 func solution(_ s:String) -> String {
     var arr = s.split(separator: " ").map{Int($0)!}
-    var result = ""
     arr = arr.sorted()
-    result.append(contentsOf: String(arr[0]))
-    result.append(" ")
-    result.append(contentsOf: String(arr[arr.count-1]))
-    return result
+    return (String(arr[0]) + " " + String(arr[arr.count-1]))
 }
