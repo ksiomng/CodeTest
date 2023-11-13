79cda55bb8bda07432dee4352a0d19c78c3d8b72
ksiomng <202024007@kangwon.ac.kr>
2023-11-13 13:03:15 +0900

    [level 2] Title: 올바른 괄호, Time: 5.68 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    var cnt = 0;
+    for i in s {
+        if i == "(" {
+            cnt += 1
+        }
+        if i == ")" {
+            cnt -= 1
+        }
+        if cnt < 0 {
+            return false
+        }
+    }
+    return cnt == 0 ? true : false
+}
