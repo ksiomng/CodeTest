c8518c7d637d8b4f6b817f671444b91691d71be5
ksiomng <202024007@kangwon.ac.kr>
2023-11-26 21:38:37 +0900

    [level unrated] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ num_list:[Int], _ n:Int) -> [Int] {
+    var result:[Int] = []
+    for i in n-1..<num_list.count {
+        result.append(num_list[i])
+    }
+    return result
+}
+
