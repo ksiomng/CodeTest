a77e6b878f058ee6cef8587cafac0507ccc6231f
ksiomng <202024007@kangwon.ac.kr>
2023-11-28 15:10:38 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.swift
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -1,23 +1,43 @@
 import Foundation
 
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    var arr = [String]()
-    var arr2 = [String]()
-    var result: Int = 0
-    let cnt = number.reduce(0, +)
-    for i in 0..<number.count {
-        for _ in 0..<number[i]{
-            arr.append(want[i])
+    let cnt = want.count
+    var wantArr = [Int](repeating: 0, count: cnt)
+    var discountArr = [Int](repeating: 0, count: cnt)
+
+    var saleDict = [String: Int]()
+
+    for i in 0..<want.count {
+        saleDict[want[i]] = i
+        wantArr[i] = number[i]
+    }
+
+    for i in 0..<10 {
+        guard let idx = saleDict[discount[i]] else {
+            continue
+        }
+        discountArr[idx] += 1
+    }
+
+    var answer = 0
+
+    if wantArr == discountArr {
+        answer += 1
+    }
+
+    for i in 0..<discount.count-10 {
+        if let idx = saleDict[discount[i]] {
+            discountArr[idx] -= 1
+        }
+
+        if let idx = saleDict[discount[i+10]] {
+            discountArr[idx] += 1
+        }
+
+        if wantArr == discountArr {
+            answer += 1
         }
     }
-    for i in 0...discount.count-cnt {
-        arr2.removeAll()
-        for j in i..<i+cnt {
-            arr2.append(discount[j])
-        }
-        if arr.sorted() == arr2.sorted() {
-            result += 1
-        }
-    }
-    return result
+
+    return answer
 }
