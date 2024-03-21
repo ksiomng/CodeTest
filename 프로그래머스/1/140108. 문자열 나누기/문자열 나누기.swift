3636d4bc1cd025d4465eecb0ce0fc89eada9c3e5
ksiomng <202024007@kangwon.ac.kr>
2024-03-21 14:45:01 +0900

    [level 1] Title: 문자열 나누기, Time: 1.11 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
@@ -1,31 +1,3 @@
-//import Foundation
-//
-//func solution(_ skill:String, _ skill_trees:[String]) -> Int {
-//    var arrs = [String]()
-//    var result : Int = 0
-//    for skill_tree in skill_trees {
-//        var a = ""
-//        for skills in skill_tree {
-//            if skill.contains(skills) {
-//                a.append(skills)
-//            }
-//        }
-//        arrs.append(a)
-//    }
-//    
-//    for arr in arrs {
-//        if arr == skill.prefix(arr.count) {
-//            result += 1
-//        }
-//    }
-//    
-//    return result
-//}
-//
-//solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"])
-
-
-
 import Foundation
 
 func solution(_ s:String) -> Int {
