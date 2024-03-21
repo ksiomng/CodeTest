433d8a98edf1f72a4c2abfa5913751254e5f1e08
ksiomng <202024007@kangwon.ac.kr>
2024-03-21 14:41:28 +0900

    [level 1] Title: 문자열 나누기, Time: 1.15 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
@@ -1,25 +1,52 @@
+//import Foundation
+//
+//func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+//    var arrs = [String]()
+//    var result : Int = 0
+//    for skill_tree in skill_trees {
+//        var a = ""
+//        for skills in skill_tree {
+//            if skill.contains(skills) {
+//                a.append(skills)
+//            }
+//        }
+//        arrs.append(a)
+//    }
+//    
+//    for arr in arrs {
+//        if arr == skill.prefix(arr.count) {
+//            result += 1
+//        }
+//    }
+//    
+//    return result
+//}
+//
+//solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"])
+
+
+
 import Foundation
 
 func solution(_ s:String) -> Int {
-    var first: String = ""
-    var fcnt: Int = 0
-    var scnt: Int = 0
     var result: Int = 0
+    
+    var first: String = ""
+    var cnt: Int = 0
+    
     for str in s {
         if first.isEmpty {
             first.append(str)
-            fcnt += 1
+            cnt += 1
         } else {
             if first == String(str) {
-                fcnt += 1
+                cnt += 1
             } else {
-                scnt += 1
+                cnt -= 1
             }
         }
-        if fcnt == scnt {
+        if cnt == 0 {
             result += 1
-            fcnt = 0
-            scnt = 0
             first = ""
         }
     }
