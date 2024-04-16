34582b927bc7a4f01350f2badc5c387899680036
ksiomng <nadana0929@gmail.com>
2024-04-16 22:17:51 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var count = 0
+
+for i in 1..<n+1 {
+    var num = i
+    while num>0, num%5 == 0 {
+    	count += 1
+        num /= 5
+    } 
+}
+
+print(count)
