31c9105076dac1c6e6738462013c80861f1bc18c
Song Kim <nadana0929@gmail.com>
2025-01-08 10:35:52 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.10 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
@@ -1,20 +1,10 @@
-import Foundation
-
-func solution(_ s:String) -> String {
-    var arr = Array(s)
-    var result = ""
-    for i in 0..<arr.count {
-        if i == 0 {
-            arr[i] = Character(String(arr[i].uppercased()))
-        }
-        else {
-            if arr[i].isUppercase {
-                arr[i] = Character(String(arr[i].lowercased()))
+func solution(_ s: String) -> String {
+    return s.split(separator: " ", omittingEmptySubsequences: false)
+            .map { word in
+                if let first = word.first {
+                    return first.isLetter ? first.uppercased() + word.dropFirst().lowercased() : word.lowercased()
+                }
+                return ""
             }
-            if arr[i-1] == " " {
-                arr[i] = Character(String(arr[i].uppercased()))
-            }   
-        }
-    }
-    return String(arr)
+            .joined(separator: " ")
 }
