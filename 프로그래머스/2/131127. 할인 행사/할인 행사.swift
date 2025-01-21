10185fd866c087d3f452e4a2e83269c805522a05
Song Kim <nadana0929@gmail.com>
2025-01-21 11:07:20 +0900

    [level 2] Title: 할인 행사, Time: 909.05 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.swift
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -1,43 +1,21 @@
 import Foundation
 
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    let cnt = want.count
-    var wantArr = [Int](repeating: 0, count: cnt)
-    var discountArr = [Int](repeating: 0, count: cnt)
-
-    var saleDict = [String: Int]()
-
+    var idx = 0
+    var arr = [String]()
+    var cnt: Int = 0
+    
     for i in 0..<want.count {
-        saleDict[want[i]] = i
-        wantArr[i] = number[i]
-    }
-
-    for i in 0..<10 {
-        guard let idx = saleDict[discount[i]] else {
-            continue
-        }
-        discountArr[idx] += 1
-    }
-
-    var answer = 0
-
-    if wantArr == discountArr {
-        answer += 1
-    }
-
-    for i in 0..<discount.count-10 {
-        if let idx = saleDict[discount[i]] {
-            discountArr[idx] -= 1
-        }
-
-        if let idx = saleDict[discount[i+10]] {
-            discountArr[idx] += 1
-        }
-
-        if wantArr == discountArr {
-            answer += 1
+        for _ in 0..<number[i] {
+            arr.append(want[i])
         }
     }
 
-    return answer
+    while idx+10 <= discount.count {
+        if discount[idx..<idx+10].sorted() == arr.sorted() {
+            cnt += 1
+        }
+        idx += 1
+    }
+    return cnt
 }
