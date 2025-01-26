3c4f24e29e508ac9ec7203d12cf2c460c0cec424
Song Kim <nadana0929@gmail.com>
2025-01-26 18:22:54 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.swift
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -1,17 +1,20 @@
 import Foundation
 
-func solution(_ clothes:[[String]]) -> Int {
-    var dic = [String:Int]()
-    var result: Int = 1
-    for clothe in clothes {
-        dic[clothe[1]] = 0
+func solution(_ clothes: [[String]]) -> Int {
+    var clothesCount: [String: Int] = [:]
+    
+    // 의상 종류별로 개수를 세기
+    for cloth in clothes {
+        let category = cloth[1]
+        clothesCount[category, default: 0] += 1
     }
-    for clothe in clothes {
-        dic[clothe[1]]! += 1
+    
+    // 조합 계산
+    var result = 1
+    for count in clothesCount.values {
+        result *= (count + 1)
     }
-    for d in dic.values {
-        result *= (d + 1)
-
-    }
+    
+    // 아무것도 입지 않는 경우 제외
     return result - 1
 }
