fe1361ffb62c8a3575ae96f9d8bd7bf5fa3b5868
Song Kim <nadana0929@gmail.com>
2025-02-04 20:04:04 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.swift
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -1,4 +1,11 @@
 func solution(_ n:Int) -> Int {
-    guard n > 0 else { return 0 }
-    return Array(1...n).filter{n % $0 == 0}.reduce(0, +)
+    var sum: Int = 0
+    if n > 0 {
+        for i in 1...n {
+            if n % i == 0 {
+                sum += i
+            }
+        }
+    }
+    return sum
 }
