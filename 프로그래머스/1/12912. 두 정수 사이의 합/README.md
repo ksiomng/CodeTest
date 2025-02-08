005cd6fa556af21799ead4a01dfc9d2c41fb6db6
Song Kim <nadana0929@gmail.com>
2025-02-08 11:30:47 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 957.10 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.1 MB, 시간: 1.54 ms
+메모리: 16.3 MB, 시간: 957.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 1:25:44
+2025년 02월 08일 11:30:45
 
 ### 문제 설명
 
--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
@@ -1,7 +1,13 @@
-// func solution(_ a:Int, _ b:Int) -> Int64 {
-//     return Int64(Array(a > b ? b...a : a...b).reduce(0, +))
-// }
-
 func solution(_ a:Int, _ b:Int) -> Int64 {
-    return Int64(a<b ? (a...b).reduce(0, +):(b...a).reduce(0, +))
+    var result : Int64 = 0
+    if b > a {
+        for i in a...b {
+            result += Int64(i)
+        }
+    } else {
+        for i in b...a {
+            result += Int64(i)
+        }
+    }
+    return result
 }
