7fda3b1dffc311c94229f198eb75bb1fe26215cd
Song Kim <nadana0929@gmail.com>
2025-02-12 13:41:37 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
@@ -1,27 +1,18 @@
 import Foundation
 
-func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
-    var idx1 = 0
-    var idx2 = 0
-    
-    for i in 0..<goal.count {
-        if goal[i] == cards1[idx1] {
-            idx1 += 1
-            if idx1 >= cards1.count {
-                idx1 = cards1.count-1
-            }
-        }
-        else {
-            if goal[i] == cards2[idx2] {
-                idx2 += 1
-                if idx2 >= cards2.count {
-                    idx2 = cards2.count-1
-                }
-            }
-            else {
-                return "No"
-            }
+func solution(_ cards1: [String], _ cards2: [String], _ goal: [String]) -> String {
+    var index1 = 0
+    var index2 = 0
+
+    for word in goal {
+        if index1 < cards1.count && cards1[index1] == word {
+            index1 += 1
+        } else if index2 < cards2.count && cards2[index2] == word {
+            index2 += 1
+        } else {
+            return "No"
         }
     }
+    
     return "Yes"
 }
