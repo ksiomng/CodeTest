0bfca27e06ccf570b2ee4773e07d8349f9dbce3b
Song Kim <nadana0929@gmail.com>
2025-02-13 22:51:46 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 26.26 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -1,3 +1,9 @@
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    return strings.sorted {Array($0)[n] == Array($1)[n] ? $0 < $1 :  Array($0)[n] < Array($1)[n]}
+    return strings.sorted(by: {
+        if Array($0)[n] == Array($1)[n] {
+            return $0 < $1
+        } else {
+            return Array($0)[n] < Array($1)[n]
+        }
+    })
 }
