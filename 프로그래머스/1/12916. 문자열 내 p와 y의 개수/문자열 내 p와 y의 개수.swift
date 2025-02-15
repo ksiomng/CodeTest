7b78ab6e0486208a872c06d98e376ac7be89fb62
Song Kim <nadana0929@gmail.com>
2025-02-15 22:59:31 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
@@ -1,4 +1,9 @@
-func solution(_ s:String) -> Bool
-{
-    return s.lowercased().filter { $0 == "p" }.count == s.lowercased().filter { $0 == "y" }.count
+import Foundation
+
+func solution(_ s: String) -> Bool {
+    let lowercasedS = s.lowercased()
+    let pCount = lowercasedS.filter { $0 == "p" }.count
+    let yCount = lowercasedS.filter { $0 == "y" }.count
+    
+    return pCount == yCount
 }
