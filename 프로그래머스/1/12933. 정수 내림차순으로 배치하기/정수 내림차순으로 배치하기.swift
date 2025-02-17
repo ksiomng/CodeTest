f18a88481a5437cc4ec960e0e81e5bbad4df8615
Song Kim <nadana0929@gmail.com>
2025-02-17 17:28:24 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.26 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.swift
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.swift
@@ -1,3 +1,11 @@
 func solution(_ n:Int64) -> Int64 {
-    return Int64(String(String(n).sorted(by: >)))!
+    var str = String(n).sorted(by: >)
+    var result:Int64 = 0
+    for s in str {
+        if result != 0 {
+            result *= 10
+        }
+        result += Int64(String(s))!
+    }
+    return result
 }
