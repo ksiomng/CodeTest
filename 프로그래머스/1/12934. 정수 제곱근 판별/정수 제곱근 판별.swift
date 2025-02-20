9d342ba295f068206ce3551121819c3a4fa114a7
Song Kim <nadana0929@gmail.com>
2025-02-20 23:45:14 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.swift
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.swift
@@ -1,12 +1,6 @@
-// func solution(_ n:Int64) -> Int64 {
-//     for i in 0...n {
-//         if (i * i == n) {return (i+1) * (i+1)}
-//     }
-//     return -1
-// }
-
 import Foundation
+
 func solution(_ n:Int64) -> Int64 {
-    let t = Int64(sqrt(Double(n))) //sqrt는 제곱근을 판별 -> 정수가 아닐수도 있으니까 더블
-    return t * t == n ? (t+1)*(t+1) : -1
+    let x = Int64(sqrt(Double(n)))
+    return (x * x == n) ? (x + 1) * (x + 1) : -1
 }
