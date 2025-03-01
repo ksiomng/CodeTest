572e204318fc86f883b88fae06b432b8b4fad2b8
Song Kim <nadana0929@gmail.com>
2025-03-01 20:33:40 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
@@ -1,7 +1,8 @@
 func solution(_ seoul:[String]) -> String {
-    return "김서방은 \((seoul.firstIndex(where: { $0 == "Kim" }))!)에 있다"
+    for i in 0..<seoul.count {
+        if seoul[i] == "Kim" {
+            return "김서방은 \(i)에 있다"
+        }
+    }
+    return ""
 }
-
-// func solution(_ seoul:[String]) -> String {
-//     return "김서방은 \(seoul.index(of: "Kim")!)에 있다"
-// }
