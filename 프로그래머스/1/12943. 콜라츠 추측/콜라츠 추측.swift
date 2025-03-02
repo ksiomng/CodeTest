360fe9eac128f98692fbbbbf5dffd3af1b74416a
Song Kim <nadana0929@gmail.com>
2025-03-02 21:20:40 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
@@ -1,12 +1,20 @@
-func solution(_ num:Int) -> Int {
-    var k = num
-    var cnt = 0
-    while (k != 1){
-    k = (k%2 == 0 ? k/2:k*3+1)
-        cnt += 1
-        if cnt >= 500 {
+func solution(_ num: Int) -> Int {
+    var count = 0
+    var n = num
+    
+    while n != 1 {
+        if count >= 500 {
             return -1
         }
+        
+        if n % 2 == 0 {
+            n /= 2
+        } else {
+            n = n * 3 + 1
+        }
+        
+        count += 1
     }
-    return cnt
+    
+    return count
 }
