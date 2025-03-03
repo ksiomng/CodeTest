83857d183cd6ca87087cd35250e6b11f8fc4aa78
Song Kim <nadana0929@gmail.com>
2025-03-03 19:23:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -1,12 +1,9 @@
 import Foundation
 
-func solution(_ numbers: [Int]) -> Int {
-    return (0...9).filter { !numbers.contains($0) }.reduce(0, +)
+func solution(_ numbers:[Int]) -> Int {
+    var result = 45
+    for num in numbers {
+        result -= num
+    }
+    return result
 }
-//func solution(_ numbers:[Int]) -> Int {
-//    var arr = [0,1,2,3,4,5,6,7,8,9]
-//    for i in 0..<numbers.count {
-//        arr[numbers[i]] = 0
-//    }
-//    return arr.reduce(0, +)
-//}
