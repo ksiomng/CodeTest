211cfc157fa5204fd66ae44c81d7a6a33120e750
Song Kim <nadana0929@gmail.com>
2025-03-06 10:48:05 +0900

    [level 2] Title: 튜플, Time: 3345.16 ms, Memory: 20.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.swift
+++ 프로그래머스/2/64065. 튜플/튜플.swift
@@ -1,54 +1,29 @@
 import Foundation
 
 func solution(_ s: String) -> [Int] {
-    var arrs = Array(s)
-    var arr = [Int]()
-    var tupleArray = [[Int]]()
-    var result = [Int]()
+    var str = s
+    str.removeFirst()
+    str.removeLast()
+
+    var arrs = [[Int]]()
     
-    arrs.removeFirst()
-    arrs.removeLast()
+    var components = str.split(separator: "}").map { $0.replacingOccurrences(of: "{", with: "").replacingOccurrences(of: ",", with: " ") }
     
-    var str = ""     // 숫자를 임시로 저장할 문자열 변수
-    var i = 0
-    
-    while true {
-        if arrs[i] == "{" {
-            while true {
-                i += 1
-                if arrs[i] == "}" {
-                    break
-                }
-                // 숫자인 경우 문자열에 추가
-                if arrs[i].isNumber {
-                    str.append(String(arrs[i]))
-                }
-                // ','를 만나면 숫자를 배열에 추가하고 임시 문자열 초기화
-                else if arrs[i] == "," {
-                    arr.append(Int(str)!)
-                    str.removeAll()
-                }
-            }
-            // 마지막 숫자를 배열에 추가하고 임시 문자열 초기화
-            arr.append(Int(str)!)
-            str.removeAll()
-            tupleArray.append(arr)              // 튜플을 배열에 추가
-            arr = []
-        } else {
-            i += 1      // '{'가 아니면 다음 위치로 이동
-        }
-        
-        if i >= arrs.count {        // 문자열 끝에 도달하면 반복 종료
-            break
+    for comp in components {
+        let numbers = comp.split(separator: " ").compactMap { Int($0) }
+        if !numbers.isEmpty {
+            arrs.append(numbers)
         }
     }
     
-    tupleArray = tupleArray.sorted(by: {$0.count < $1.count})
+    arrs.sort { $0.count < $1.count }
     
-    for tups in tupleArray {
-        for tup in tups {
-            if !result.contains(tup) {
-                result.append(tup)
+    var result = [Int]()
+    
+    for arr in arrs {
+        for num in arr {
+            if !result.contains(num) {
+                result.append(num)
             }
         }
     }
