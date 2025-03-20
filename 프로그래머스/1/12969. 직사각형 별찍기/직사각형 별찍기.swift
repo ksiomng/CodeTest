c186a8673896ed3f6bc993bce1d5f5aaa8b323cb
Song Kim <nadana0929@gmail.com>
2025-03-20 16:05:45 +0900

    [level 1] Title: 직사각형 별찍기, Time: 26.81 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -3,6 +3,9 @@ import Foundation
 let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 let (a, b) = (n[0], n[1])
 
-for _ in 1...b {
-    print(String(repeating: "*", count: a))
+for i in 0..<n[1] {
+    for j in 0..<n[0] {
+        print("*", terminator: "")
+    }
+    print("")
 }
