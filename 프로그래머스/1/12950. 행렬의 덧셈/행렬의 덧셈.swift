197cf1d84c654ddf2fe717fcf61a9b7ded337405
Song Kim <nadana0929@gmail.com>
2025-03-20 16:00:47 +0900

    [level 1] Title: 행렬의 덧셈, Time: 15.91 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
@@ -1,21 +1,9 @@
-import Foundation
-//
-// < 처음풀이 >
 func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-   var answer1 : [Int] = []
-   var answer2 : [[Int]] = []
-
-   for i in 0..<arr1.count {
-       for j in 0..<arr1[i].count {
-           answer1.append(arr1[i][j]+arr2[i][j])
-       }
-       answer2.append(answer1)
-       answer1 = []
-   }
-   return answer2
+    var result = arr1
+    for i in 0..<arr1.count {
+        for j in 0..<arr1[i].count {
+            result[i][j] = arr1[i][j] + arr2[i][j]
+        }
+    }
+    return result
 }
-//
-// < 리팩토링 >
-// func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-//    return zip(arr1, arr2).map{zip($0,$1).map{$0+$1}}
-// }
