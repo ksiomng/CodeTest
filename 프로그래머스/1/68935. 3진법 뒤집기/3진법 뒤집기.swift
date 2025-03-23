48de55cda1ea85543758bffba65e17b6fe4707dd
Song Kim <nadana0929@gmail.com>
2025-03-23 23:54:36 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,5 +1,18 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
-    return Int(String((String(n, radix: 3)).reversed()), radix: 3)!
+func solution(_ n: Int) -> Int {
+    // 1. 10진수를 3진법 문자열로 변환
+    var ternary = ""
+    var num = n
+    
+    while num > 0 {
+        ternary = String(num % 3) + ternary
+        num /= 3
+    }
+    
+    // 2. 3진법 문자열을 뒤집기
+    let reversedTernary = String(ternary.reversed())
+    
+    // 3. 뒤집은 3진법 문자열을 다시 10진수로 변환
+    return Int(reversedTernary, radix: 3) ?? 0
 }
