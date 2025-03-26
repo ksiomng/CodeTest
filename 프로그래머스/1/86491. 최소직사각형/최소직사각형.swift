1495f535f9b7a555c113fef4e5581d2029fbb09c
Song Kim <nadana0929@gmail.com>
2025-03-26 21:22:38 +0900

    [level 1] Title: 최소직사각형, Time: 1.51 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -1,38 +1,16 @@
 import Foundation
-//
-// < 처음풀이 >
-//func solution(_ sizes:[[Int]]) -> Int {
-//   var max = 0
-//   var min = 0
-//   for i in 0..<sizes.count {
-//       if sizes[i][0] > sizes[i][1] {
-//           if (max < sizes[i][0]) {
-//               max = sizes[i][0]
-//           }
-//           if (min < sizes[i][1]) {
-//               min = sizes[i][1]
-//           }
-//       }
-//       else {
-//           if (max < sizes[i][1]) {
-//               max = sizes[i][1]
-//           }
-//           if (min < sizes[i][0]) {
-//               min = sizes[i][0]
-//           }
-//       }
-//   }
-//   return (max * min)
-//}
 
-// < 리팩토링 >
-func solution(_ sizes:[[Int]]) -> Int {
-  var maxNum = 0
-  var minNum = 0
+func solution(_ sizes: [[Int]]) -> Int {
+    var maxWidth = 0
+    var maxHeight = 0
 
-  for size in sizes {
-      maxNum = max(maxNum, size.max()!)
-      minNum = max(minNum, size.min()!)
-  }
-  return maxNum * minNum
+    for size in sizes {
+        let (w, h) = (size[0], size[1])
+        let (longer, shorter) = w > h ? (w, h) : (h, w)
+
+        maxWidth = max(maxWidth, longer)
+        maxHeight = max(maxHeight, shorter)
+    }
+
+    return maxWidth * maxHeight
 }
