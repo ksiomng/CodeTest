571fd86949f9f666693ae2a8306709817bf1a8a2
Song Kim <nadana0929@gmail.com>
2025-03-28 11:45:08 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,22 +1,14 @@
 import Foundation
-//
-// < 처음풀이 >
-//func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-//    var result = [Int]()
-//    for i in 0..<commands.count {
-//        var temp = [Int]()
-//        for j in (commands[i][0]-1)...(commands[i][1]-1) {
-//            temp.append(array[j])
-//        }
-//        temp = temp.sorted()
-//        result.append(temp[(commands[i][2]-1)])
-//    }
-//    return result
-//}
 
-// < 리팩토링 >
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    return commands.map({(key) in
-        return array[(key[0]-1)...(key[1]-1)].sorted()[key[2]-1]
-    })
+    var result = [Int]()
+    for i in 0..<commands.count {
+        var arr = [Int]()
+        for j in (commands[i][0]-1)...(commands[i][1]-1) {
+            arr.append(array[j])
+            arr = arr.sorted()
+        }
+        result.append(arr[commands[i][2]-1])
+    }
+    return result
 }
